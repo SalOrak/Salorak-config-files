2ca501b343e0c92f786d0e546fb4d36fd014cd03
salorak <salorack@protonmail.com>
2023-05-02 23:29:05 +0200

    Added remaps and netrw liststyle

--- Configurations/nvim_lua/lua/user/init.lua
+++ Configurations/nvim_lua/lua/user/init.lua
@@ -3,5 +3,6 @@ require("user.packer")
 require("user.set")
 
 vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
+vim.g.netrw_banner = 0 -- Remove banner
+vim.g.netrw_liststyle = 3
 vim.g.netrw_netrw_winsize = 25
--- Configurations/nvim_lua/lua/user/remap.lua
+++ Configurations/nvim_lua/lua/user/remap.lua
@@ -1,12 +1,18 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
 
+
 vim.keymap.set("n", "J", "mzJ`z")
 
+-- Keep centered while scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+
+-- Copy function using shortcut
+vim.keymap.set("n", "yaf", "va{Vy}")
+vim.keymap.set("n", "yif", "vi{Vy}")
+
+-- Remap jk to Esc (enter normal mode without pressing Esc)
 vim.keymap.set("i", "jk", "<Esc>")
 
-vim.keymap.set("n", "yaf", "va{Vy")
-vim.keymap.set("n", "yif", "vi{Vy")
