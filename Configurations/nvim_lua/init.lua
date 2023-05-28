393670f1fee661489b387d5ddab27d55fb2ef56b
salorak <salorack@protonmail.com>
2023-05-28 13:24:52 +0200

    feat: highlight yank whenever yank

--- Configurations/nvim_lua/init.lua
+++ Configurations/nvim_lua/init.lua
@@ -1,3 +1,20 @@
 require("user")
 
 
+-- Highlight when yank 
+local augroup= vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank',{})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 50
+        })
+    end,
+})
+
+
